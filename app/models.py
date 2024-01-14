8330ab6eb77491557842c4ab523b71868c231412
joshua chivile <joshuachivile@gmail.com>
2024-01-15 00:29:39 +0300

    Added Hero Model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+db = SQLAlchemy(metadata=metadata)
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+    powers = db.relationship('HeroPower', backref='hero')
+
+    serialize_rules = ('-powers.hero',)
+
+    def __repr__(self):
+        return f'<Hero id: {self.id} , name is {self.name} and alias is {self.super_name} >'
